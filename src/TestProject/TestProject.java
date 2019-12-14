07759da1f14a4cabccf4c3cb920909b4cc7a43df
Shweta-Dhondwad <dhondvadshweta08@gmail.com>
2019-12-13 20:58:33 -0800

    first commit

---
+++ src/TestProject/TestProject.java
@@ -0,0 +1,14 @@
+package TestProject;
+
+public class TestProject {
+
+	public TestProject() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
+}
